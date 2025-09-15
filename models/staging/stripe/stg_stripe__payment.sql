678827182fd0d5908d3bf1c30528f3acd0ac3624
Sam Okeowo <stablellc4@gmail.com>
2025-09-15 01:39:34 +0000

    added singular test to stg_stripe__payment

--- models/staging/stripe/stg_stripe__payment.sql
+++ models/staging/stripe/stg_stripe__payment.sql
@@ -12,9 +12,9 @@ renamed as (
         id as payment_id,
         orderid as order_id,
         paymentmethod as payment_method,
-        status,
-        amount / 100 as amount,
-        created as created_at,
+        status as payment_status,
+        amount,
+        created,
         _batched_at
 
     from source
